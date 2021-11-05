df67b3bba4b75b4f9a79b35ee18f315e6ccebdac
Henry Chungit So <hso28@uwo.ca>
2021-11-05 19:22:15 -0400

    StatsObject C++ file and StatsObject header file

---
+++ StatsObject.cpp
@@ -0,0 +1,60 @@
+//Henry So
+//CS3307 StatsObject C++ file
+//Nov 5 2021
+
+//include header file
+#include "StatsObject.h"
+
+
+/**
+ * Constructor for Command class; sets the instance variables to default values.
+ */
+StatsObject::StatsObject(){
+    this->wpm = 0;
+    this->totalGames = 0;
+    this->totalWords = 0;
+    this->totalChars = 0;
+    this->totalWrongChars = 0;
+    this->accuracy = 0.0;
+}
+
+/**
+ * Destructor for StatsObject class; here to allow for deletion of class when keyword delete is used.
+ */
+StatsObject::~StatsObject(){
+}
+
+/**
+ * Setter method for variable wpm. Using the parameter wpm, this object's instance of wpm will be set to the whatever is passed in the parameter.
+ */
+void StatsObject::setWpm (int wpm){
+    this->wpm = wpm;
+}
+
+/**
+ * Setter method for variable totalGames. Using the parameter totalGames, this object's instance of totalGames will be set to the whatever is passed in the parameter.
+ */
+void StatsObject::setTotalGames (int totalGames){
+    this->totalGames = totalGames;
+}
+
+/**
+ * Setter method for variable totalWords. Using the parameter totalWords, this object's instance of totalWords will be set to the whatever is passed in the parameter.
+ */
+void StatsObject::setTotalWords (int totalWords){
+    this->totalWords = totalWords;
+}
+
+/**
+ * Setter method for variable totalWrongChars. Using the parameter totalWrongChars, this object's instance of totalWrongChars will be set to the whatever is passed in the parameter.
+ */
+void StatsObject::setTotalWrongChars(int totalWrongChars){
+    this->totalWrongChars = totalWrongChars;
+}
+
+/**
+ * Setter method for variable accuracy. Using the parameter accuracy, this object's instance of accuracy will be set to the whatever is passed in the parameter.
+ */
+void StatsObject:: setAccuracy(float accuracy){
+    this->accuracy = accuracy;
+}
---
+++ StatsObject.h
@@ -0,0 +1,92 @@
+//Henry So
+//CS3307 StatsObject Header File
+// Nov 5 2021
+
+//guard
+#ifndef StatsObject_h
+#define StatsObject_h
+
+
+class StatsObject {
+    private:
+    //instance variables
+    int wpm;
+    int totalGames;
+    int totalWords;
+    int totalChars;
+    int totalWrongChars;
+    float accuracy;
+
+    public:
+    /**
+     * Constructor for StatsObject class; sets the variables to default values
+     */
+    StatsObject();
+
+    /**
+     * Destructor for StatsObject class
+     */
+    ~StatsObject();
+
+    /** 
+     * getter method for variable wpm; returns int
+    */ 
+    int getWpm() { return wpm; }
+    
+    /** 
+     * getter method for variable totalGames; returns int
+    */ 
+    int getTotalGames() { return totalGames; }
+    
+    /** 
+     * getter method for variable totalWords; returns int
+    */ 
+    int getTotalWords() { return totalWords; }
+    
+    /** 
+     * getter method for variable totalChars; returns int
+    */ 
+    int getTotalChars() { return totalChars; }
+    
+    /** 
+     * getter method for variable totalWrongChars; returns int
+    */ 
+    int getTotalWrongChars() { return totalWrongChars; }
+   
+    /** 
+     * getter method for variable accuracy; returns float
+    */ 
+    float getAccuracy() { return accuracy; }
+    
+    /**
+    * Setter method for variable wpm. Using the parameter wpm, this objects wpm variable is set to whatever is in the parameter variable.
+    */
+    void setWpm(int wpm);
+
+    /**
+    * Setter method for variable totalGames. Using the parameter totalGames, this objects totalGames variable is set to whatever is in the parameter variable.
+    */
+    void setTotalGames(int totalGames);
+
+    /**
+    * Setter method for variable totalWords. Using the parameter totalWords, this objects totalWords variable is set to whatever is in the parameter variable.
+    */
+    void setTotalWords(int totalWords);
+
+    /**
+    * Setter method for variable totalChars. Using the parameter totalChars, this objects totalChars variable is set to whatever is in the parameter variable.
+    */
+    void setTotalChars(int totalChars);
+
+    /**
+    * Setter method for variable totalWrongChars. Using the parameter totalWrongChars, this objects totalWrongChars variable is set to whatever is in the parameter variable.
+    */
+    void setTotalWrongChars(int totalWrongChars);
+
+    /**
+    * Setter method for variable accuracy. Using the parameter accuracy, this objects accuracy variable is set to whatever is in the parameter variable.
+    */
+    void setAccuracy(float accuracy);
+};
+
+#endif
